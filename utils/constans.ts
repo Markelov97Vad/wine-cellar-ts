914777c19136dcb56d5faba3b569cf47f59c5695
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-10-26 21:56:32 +0300

    merge feat/nextjs

--- utils/constans.ts
+++ utils/constans.ts
@@ -1,5 +1,5 @@
-export const MAIN_API_URL = 'http://localhost:3005';
-// export const MAIN_API_URL = 'https://api-my-wine-cellar.space';
+// export const MAIN_API_URL = 'http://localhost:3005';
+export const MAIN_API_URL = 'https://api-my-wine-cellar.space';
 
 export const headersData = (token: string) => {
   return {
